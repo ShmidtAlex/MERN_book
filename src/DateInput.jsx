fa0acbb195b28145bfd04e03f4c85835cf3acbbe
Alexander Shmidt <shmidt.alex@gmail.com>
2018-06-13 19:26:59 +0300

    DateInput.jsx is commented, IssueEdit corrected considering DateInput component, errors got their own styles in style section index.html.

--- src/DateInput.jsx
+++ src/DateInput.jsx
@@ -4,32 +4,42 @@ import PropTypes from 'prop-types';
 export default class DateInput extends React.Component {
   constructor(props) {
     super(props);
-    this.state. = { value: this.editFormat(props.value), focused: false, valid: true };
+    console.log(props)
+    //state for date field initially not focused, and valid, because of shows valid data
+    this.state = { value: this.editFormat(props.value), focused: false, valid: true };
     this.onFocus = this.onFocus.bind(this);
     this.onBlur = this.onBlur.bind(this);
     this.onChange = this.onChange.bind(this);
   }
   componentWillReceiveProps(newProps) {
+  //newProps here is edited by user date field. if it was editet, this.state knows about this new values
     if (newProps.value !== this.props.value) {
       this.setState({ value: this.editFormat(newProps.value) });
     }
   }
   onFocus() {
+    //this one only changed the focused value in this.state, when user clicked on date field
     this.setState({ focused: true });
   }
   onBlur(e) {
     const value = this.unformat(this.state.value);
     const valid = this.state.value === '' || value != null;
+    //if this.state.value != '' && this.unformat(this.state.value) != null
+    //and also if there is any value in onValidityChange prop from parent component
     if (valid !== this.state.valid && this.props.onValidityChange) {
+      //that parent component recieves two arguments, including 'valid' (this.unformat(this.state value))
       this.props.onValidityChange(e, valid);
     }
+    //then we miss focuse, and confirm that valid is true
     this.setState({ focused: false, valid });
     if (valid) {
+      console.log(value);
       this.props.onChange(e, value);
     }
   }
   onChange(e) {
-    if (e.target.value.match(/^\[d-]*S/)) {
+    //if new value from user corresponding to RegExp, this value can exist in this field, otherwise it even doesn't typing in
+    if (e.target.value.match(/^[\d-]*$/)) {
       this.setState({ value: e.target.value });
     }
   }
@@ -37,13 +47,16 @@ export default class DateInput extends React.Component {
     return (date != null) ? date.toDateString() : '';
   }
   editFormat(date) {
+    //if there is any date, convert it to string ISO standart from 0 index to 9 (10 symb.length)
     return (date != null) ? date.toISOString().substr(0, 10) : '';
   }
   unformat(str) {
+    //this one check is new unsetted state is a number, and if it is, allows it exist in this field
     const val = new Date(str);
     return isNaN(val.getTime()) ? null : val;
   }
   render() {
+  {/*define the conditions for clasName='invalid' (for next styling it and showing to user, that it is)*/}
     const className = (!this.state.valid && !this.state.focused) ? 'invalid' : null;
     const value = (this.state.focused || !this.state.valid) ? this.state.value : this.displayFormat(this.props.value);
     return (
--- src/IssueEdit.jsx
+++ src/IssueEdit.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router';
 import PropTypes from 'prop-types';
 import NumInput from './NumInput.jsx';
+import DateInput from './DateInput.jsx';
 
 export default class IssueEdit extends React.Component {
   constructor() {
@@ -10,11 +11,14 @@ export default class IssueEdit extends React.Component {
       issue: {
         //these are the properties of issue object and names for each inputs both
         _id:'', title: '', status: '', owner: '', effort: null, 
-        completionDate: '', created: '',
+        completionDate: null, created: '',
       },
+      invalildFields: {},
     };
     this.onChange = this.onChange.bind(this);
+    this.onValidityChange = this.onValidityChange.bind(this);
   }
+  
   componentDidMount() {
     this.loadData();
   }
@@ -23,16 +27,29 @@ export default class IssueEdit extends React.Component {
       this.loadData();
     }
   }
+  //we got 'valid' argument from child component
+  onValidityChange(event, valid) {
+    const invalildFields = Object.assign({}, this.state.invalildFields);
+    //in case, where user typed any wrong value,
+    if (!valid) {
+      invalildFields[event.target.name] = true;
+      console.log(event.target);
+    } else {
+      delete invalildFields[event.target.name];
+    }
+    this.setState({ invalildFields });
+  }
   //this works only without submit yet
   onChange(event, convertedValue){
     //clone state object to empty object 'issue' for the purpose of access to the 'name' properties of inputs
     const issue = Object.assign({}, this.state.issue);
     const value = (convertedValue !== undefined ? convertedValue : event.target.value);
-    //f.e.g: event.target.name = status, event.target.value = New; 
+    //f.e.g: event.target.name = status, event.target.value = New or date:'some date in string format'; 
     //we use target's name as a key in the state object to set the value in the state object
    issue[event.target.name] = value;
     //change this.state object conserning to new status
     this.setState({ issue });
+
   }
   loadData() {
     //this.props.params.id means the issue id
@@ -42,10 +59,9 @@ export default class IssueEdit extends React.Component {
           //convert date to string
           issue.created = new Date(issue.created).toDateString();
           //if issue.comletionDate NOT equal null, convert given new Date object to string, else - leave it empty
-          issue.completionDate = issue.completionDate != null ? new Date(issue.completionDate).toDateString() : '';
+          issue.completionDate = issue.completionDate != null ? new Date(issue.completionDate) : null;
           //change this.state accordingly converted data
           this.setState({ issue });
-          console.log(issue);
         });
       } else {
         response.json().then(error => {
@@ -58,6 +74,8 @@ export default class IssueEdit extends React.Component {
   }
   render() {
     const issue = this.state.issue;
+    const validationMessage = Object.keys(this.state.invalildFields).length === 0 ? null : (<div className="error"
+    >Please correct invalid fields before submitting.</div>);
     return (
       <div>
         <form>
@@ -81,10 +99,12 @@ export default class IssueEdit extends React.Component {
           <br/>
           Effort: <NumInput size={5} name="effort" value={issue.effort} onChange={this.onChange}/>
           <br/>
-          Completion Date: <input name="completionDate" value={issue.completionDate} onChange={this.onChange}/>
+          Completion Date: <DateInput name="completionDate" value={issue.completionDate} onChange={this.onChange}
+          onValidityChange={this.onValidityChange}/>
           <br/>
           Title: <input name="title" size={50} value={issue.title} onChange={this.onChange}/>
           <br/>
+          {validationMessage}
           <button type="submit">Submit</button>
           <Link to="/issues">Back to issue list</Link>
         </form>
