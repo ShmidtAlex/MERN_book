a353079a92103868a0ba65cabaad91aec7040406
Alexander Shmidt <shmidt.alex@gmail.com>
2018-06-14 21:58:31 +0300

    created Header component in App.jsx, wich now uses instead last one hard-coded header, imported all that we need from react bootstrap and react-router-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import 'babel-polyfill';
 import React from 'react';
 import { Router, Route, Redirect, browserHistory, withRouter } from 'react-router';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Glyphicon } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import IssueList from './IssueList.jsx';
@@ -8,12 +10,30 @@ import IssueEdit from './IssueEdit.jsx';
 
 const contentNode = document.getElementById('contents');
 const NoMatch = () => <p>Page Not found</p>;
-
+const Header = () => (
+  <Navbar fluid>
+    <Navbar.Header>
+      <Navbar.Brand>Issue Tracker</Navbar.Brand>
+    </Navbar.Header>
+    <Nav>
+      <LinkContainer to="/issues">
+        <NavItem>Issues</NavItem>
+      </LinkContainer>
+      <LinkContainer to="/reports">
+        <NavItem>Reports</NavItem>
+      </LinkContainer>
+    </Nav>
+    <Nav pullRight>
+      <NavItem><Glyphicon glyph="plus" />Create Issue</NavItem>
+      <NavDropdown id="user-dropdown" title={<Glyphicon glyph="option-horizontal"/>} noCaret>
+        <MenuItem>Logout</MenuItem>
+      </NavDropdown>
+    </Nav>
+  </Navbar>
+);
 const App = (props) => (
   <div>
-    <div className="header">
-      <h1>Issue Tracker</h1>
-    </div>
+    <Header />
     <div className="container-fluid">
       {props.children}
     </div>
