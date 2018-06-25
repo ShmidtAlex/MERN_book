1e515e2dd950a835de0454ef5db322d6f3e2fa98
Alexander Shmidt <shmidt.alex@gmail.com>
2018-06-25 19:15:03 +0300

    client/Client.jsx changed for rendering routes from Routes.jsx, app shows issues and work well

--- client/Client.jsx
+++ client/Client.jsx
@@ -1,10 +1,22 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWorld from '../src/HelloWorld.jsx';
+import { Router, browserHistory } from 'react-router';
+
+import routes from '../src/Routes.jsx';
+import ContextWrapper from '../src/ContextWrapper.jsx';
+
+const WrappedApp = (props) => (
+  <ContextWrapper {...props}>
+    <Router history={browserHistory}>
+      {routes}
+    </Router>
+  </ContextWrapper>
+);
 
 const contentNode = document.getElementById('contents');
-ReactDOM.render(<HelloWorld {...window.__INITIAL_STATE__}/>, contentNode);
+ReactDOM.render(<WrappedApp initialState={window.__INITIAL_STATE__}/>, 
+  contentNode);
 
 if(module.hot) {
   module.hot.accept();
