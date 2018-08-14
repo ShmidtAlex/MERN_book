cf5c1f624c5780a21e9bf5e31412c664b64f0e4f
Alexander Shmidt <shmidt.alex@gmail.com>
2018-08-14 22:36:25 +0300

    App changed to statefull component for Google sign in form

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,38 @@ import PropTypes from 'prop-types';
 import Header from './Header.jsx';
 
 
-const App = (props) => (
-  <div>
-    <Header />
-    <div className="container-fluid">
-      {props.children}
-    {/*props.children consists history, location, params, route's path, routes*/}
-      <hr />
-      <h5><small>
-        Full source code available at this <a href="https://github.com/vasansr/pro-mern-stack">
-        GitHub repository</a>.
-      </small></h5>
-    </div>
-  </div>
-);
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: { signedIn: false, name: ''},
+    };
+    this.onSingin = this.onSingin.bind(this);
+    this.onSingout = this.onSingout.bind(this);
+    onSingin(name) {
+      this.setState({ user: { signedIn: true, name } });
+    }
+    render() {
+      return (
+        <div>
+          <Header />
+          <div className="container-fluid">
+            {props.children}
+          {/*props.children consists history, location, params, route's path, routes*/}
+            <hr />
+            <h5><small>
+              Full source code available at this <a href="https://github.com/vasansr/pro-mern-stack">
+              GitHub repository</a>.
+            </small></h5>
+          </div>
+        </div>
+      );
+    }
+    
+  }
+} 
 
 App.propTypes = {
   children: PropTypes.object.isRequired,
 };
 
-export default App;
